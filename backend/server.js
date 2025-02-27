c96a82be69ea632872b6137c1e7d264927c73177
aamena <aamena.rangwala@tectrionix.com>
2025-02-27 16:27:25 +0530

    simplify cors

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,18 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 // const allowedOrigins = process.env.FRONTEND_URL?.split(',');
 
+// const io = new Server(server, {
+//     cors: {
+//         origin: allowedOrigins || "*",
+//         credentials: true
+//     }
+// });
+
 const io = new Server(server, {
     cors: {
         origin: '*'
     }
 });
-
 
 io.use(async (socket, next) => {
     try {
